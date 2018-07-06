378b2dd6a4bd7e22d1aa36e68e5d85b39c8c8e80
Tao <werewaswas@live.com>
2018-07-06 23:31:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,11 +6,11 @@ class TicTacToe
 
   # Helper Methods
   def display_board
-    puts " #{@@board[0]} | #{@@board[1]} | #{@@board[2]} "
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
-    puts " #{@@board[3]} | #{@@board[4]} | #{@@board[5]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
-    puts " #{@@board[6]} | #{@@board[7]} | #{@@board[8]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
   def input_to_index(user_input)
@@ -18,13 +18,13 @@ class TicTacToe
   end
 
   def move(index, current_player)
-    @@board[index] = current_player
+    @board[index] = current_player
   end
 
 
 
   def valid_move?(index)
-    index.between?(0,8) && !position_taken?(@@board, index)
+    index.between?(0,8) && !position_taken?(@board, index)
   end
 
   def turn
